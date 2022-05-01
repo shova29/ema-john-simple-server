964d3d19b0546e2c9a9666319482eab81fcb06a6
shova29 <halima2996@gmail.com>
2022-05-01 22:35:39 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
 
 //middleware
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ydrjm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+/* client.connect((err) => {
+  const collection = client.db("emaJohn").collection("product");
+  // perform actions on the collection object
+  console.log("Mongodb is connected");
+  client.close();
+} */
 
 app.get("/", (req, res) => {
   res.send("Ema-John Server is running");
